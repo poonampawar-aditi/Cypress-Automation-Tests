2b3792f85e2789c081bdd7bbef43542ff64fe111
poonampawar-aditi <aditi.bhosale2013@gmail.com>
2025-12-09 02:08:35 +0100

    Refactor Cypress configuration to update projectId and enhance setupNodeEvents

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,61 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'upboiu',
+  // projectId: 'upboiu',
+  projectId: '32bd89',
   e2e: {
-   
-    watchForFileChanges: false, //to disable auto re-run of tests on saving the file
+    url: 'https://rahulshettyacademy.com',
+       watchForFileChanges: false, //to disable auto re-run of tests on saving the file
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     specPattern: 'cypress/e2e/examples/*.cy.js'
   },
 });
+
+
+const { defineConfig } = require("cypress");
+const browserify = require("@cypress/browserify-preprocessor");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  preprendTransformerToOptions,
+} = require("@badeball/cypress-cucumber-preprocessor/browserify");
+ 
+ async function setupNodeEvents(on,config)
+ {
+  await addCucumberPreprocessorPlugin(on, config);
+ 
+  on(
+    "file:preprocessor",
+    browserify(preprendTransformerToOptions(config, browserify.defaultOptions)),
+  );
+ 
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+ }
+ 
+ 
+module.exports = defineConfig({
+  defaultCommandTimeout: 6000,
+ 
+  env: {
+    url: "https://rahulshettyacademy.com",
+  },
+  reporter: 'cypress-mochawesome-reporter',
+ 
+  retries: {
+    runMode: 1,
+ 
+  },
+  projectId: "32bd89",
+ 
+ 
+  e2e: {
+  setupNodeEvents,
+    specPattern: 'cypress/integration/examples/*.js'
+ 
+  },
+});
+ 
